340e16a1257af01730ba1ab1ad38ea0a166368a6
Sidney2598 <141900433+Sidney2598@users.noreply.github.com>
2025-04-27 15:49:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,29 @@ import streamlit as st
 import pathlib
 import plotly.express as px
 import platform
-from fastai.learner import load_learner
-plt=platform.system()
-if plt=='Linux':pathlib.WindowsPath=pathlib.PosixPath
 
+# Linux server uchun WindowsPath to PosixPath
+if platform.system() == 'Linux':
+    pathlib.WindowsPath = pathlib.PosixPath
+
+# Modelni dastur boshlanishida yuklab olamiz
+model = load_learner('transport_1_loyiha.pkl', cpu=True)
 
 st.title('Nihoyat ishladi')
-file=st.file_uploader('Rasm yuklash',type=['png','jpeg','gif','svg'])
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+
 if file:
     st.image(file)
-    #PIL CONVERT
-    img=PILImage.create(file)
-    #model
-    model=load_learner('transport_1_loyiha.pkl',cpu=True)
 
-    predict, pred_id, probs=model.predict(img)
-    st.success(f'Bashorat:{predict}')
-    st.info(f'Ehtimollik:{probs[pred_id]*100:.1f}%')    
-    #plotly
-    fig=px.bar(x=probs,y=model.dls.vocab)
+    # Rasmni PIL formatga o'zgartirish
+    img = PILImage.create(file)
+
+    # Modeldan bashorat olish
+    predict, pred_id, probs = model.predict(img)
+
+    st.success(f'Bashorat: {predict}')
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+
+    # Ehtimolliklarni chizish
+    fig = px.bar(x=probs, y=model.dls.vocab)
     st.plotly_chart(fig)
