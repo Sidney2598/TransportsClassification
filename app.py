5d4eb92d7445b32db59ef63e3b063eef508da557
Sidney2598 <141900433+Sidney2598@users.noreply.github.com>
2025-04-27 15:13:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pathlib
 import plotly.express as px
 import platform
+from fastai.learner import load_learner
 plt=platform.system()
 if plt=='Linux':pathlib.PosixPath=pathlib.WindowsPath
 
@@ -13,7 +14,7 @@ if file:
     #PIL CONVERT
     img=PILImage.create(file)
     #model
-    model=load_learner('transport_1_loyiha.pkl')
+    model=load_learner('transport_1_loyiha.pkl',cpu=True)
 
     predict, pred_id, probs=model.predict(img)
     st.success(f'Bashorat:{predict}')
